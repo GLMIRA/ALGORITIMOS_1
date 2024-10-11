7bc6bf679d621cb169159c33543ad42d160747bc
Eric Borges <borgeseric391@gmail.com>
2024-10-11 15:17:24 -0300

    Neste codigo eu fiz uma formula de lancamento obliquo

--- modulo_1/lancamento_obliquo_borges.c
+++ modulo_1/lancamento_obliquo_borges.c
@@ -1,116 +1,104 @@
-/******************************************************************************
-
-                            Online C Compiler.
-                Code, Compile, Run and Debug C program online.
-Write your code in this editor and press "Run" button to compile and execute it.
-
-*******************************************************************************/
-
-#include <stdio.h>
+#include <stdio.h>											
 #include <math.h>
 
-/* primeiro exercicio, aqui ele recebe o tempo do local em segundos e transforma em hora e minutos
-float main() {
-	float tempolocal,hora,minu,segun;
-	  		printf("Digite o tempo em segundos!\n");
-                scanf("%f",&tempolocal);
-    if(tempolocal > 0){
-    minu = tempolocal / 60;            
-    hora = minu/ 60;   
-        printf("Tempo em horas: %f hora, %f minutos", hora, minu);
-    }else{printf("O Tempo não pode ser 0 ou menor !");}
-  return 0;
-	
-}*/
-/* isso é uma serie de switch onde seleciona o número de medias que vc quiser 
-float main(){
-    float n1,n2,n3,r;
-    int c;
-        printf("Quantos valores tera na média?");
-            scanf("%d", &c);
-        
-    switch (c)
+
+
+const double pi = 3.14159265;
+double tempo,delta_tempo,velocidade_vertical,velocidade_horizontal,velocidade_inical,altura,distancia,gravidade,angulo,resultado;
+int escolha;
+
+double formula_cos(double angulo){
+    angulo = angulo*(pi/180);
+    angulo = cos(angulo);
+    return (angulo);
+}
+double formula_sen(double angulo){
+    angulo = sin(angulo*pi/180);
+    return (angulo);
+}
+
+double formula_velocidade_vertical(double velocidade_inical, double angulo){
+    velocidade_vertical = velocidade_inical*angulo;
+    return (velocidade_vertical);
+}
+
+double formula_tempo_subida(double velocidade_vertical, double gravidade){
+    tempo = velocidade_vertical/gravidade;
+    return (tempo);
+    
+}
+
+double formula_velocidade_horizontal(double velocidade_inical, double angulo){
+    velocidade_horizontal = velocidade_inical*angulo;
+    return (velocidade_horizontal);
+}
+
+double formula_altura_maxima(double velocidade_vertical, double gravidade){
+    altura = pow(velocidade_vertical,2)/(2*gravidade);
+    return (altura);
+}
+
+double formula_delta_tempo(double tempo){
+    tempo = (tempo*2);
+    return (tempo);
+}
+
+double formula_distancia(double velocidade_horizontal, double delta_tempo, double angulo){
+    distancia = velocidade_horizontal*delta_tempo;
+    return(distancia);
+}
+
+double main()
+{
+printf("Caso queira calcular o tempo de subida digite 1, altura maxima 2, alcance 3, tempo total 4\n");
+scanf("%d",&escolha);
+    switch(escolha)
     {
-        case 1:
-            printf("Digite o primeiro valor:\n");
-                scanf("%f", &n1);
-                    r = n1/c; 
-                        printf("O valor da Média é %f ",r);
-        break;
-        
-        case 2:
-            printf("Digite o primeiro valor:\n");
-                scanf("%f", &n1);
-            printf("Digite o segundo valor:\n");
-                scanf("%f", &n2);
-                    r = (n1+n2)/c; 
-                        printf("O valor da Média é %f ",r);
-        break;
-        
-        case 3:
-            printf("Digite o primeiro valor:\n");
-                scanf("%f", &n1);
-            printf("Digite o segundo valor:\n");
-                scanf("%f", &n2);
-            printf("Digite o terceiro valor:\n");
-                scanf("%f", &n3);
-                    r = (n1+n2+n3)/c; 
-                        printf("O valor da Média é %f ",r);
-        break;
-        
-        
-        
+    case 1:
+    printf("Qual o valor da velocidade inical\n");
+    scanf("%lf",&velocidade_inical);
+    printf("Qual o valor do angulo\n");
+    scanf("%lf",&angulo);
+    printf("Qual o valor da gravidade\n");
+    scanf("%lf",&gravidade);
+    angulo = formula_sen(angulo);
+    velocidade_vertical = formula_velocidade_vertical(velocidade_inical,angulo);
+    tempo = formula_tempo_subida(velocidade_vertical,gravidade);
+    printf("O tempo de subida é %.2lf, sua velocidade vertical foi de : %.2lf\n",tempo,velocidade_vertical);
+    break;    
+    
+    case 2:
+    printf("Qual o valor da velocidade inical\n");
+    scanf("%lf",&velocidade_inical);
+    printf("Qual o valor do angulo\n");
+    scanf("%lf",&angulo);
+    printf("Qual o valor da gravidade\n");
+    scanf("%lf",&gravidade);
+    angulo = formula_sen(angulo);
+    velocidade_vertical = formula_velocidade_vertical(velocidade_inical,angulo);
+    altura = formula_altura_maxima(velocidade_vertical,gravidade);
+    printf("Resultado é: %.2lf\n",altura);
+    break;
+    
+    case 3:
+    printf("Qual o valor da velocidade inical\n");
+    scanf("%lf",&velocidade_inical);
+    printf("Qual o valor do angulo\n");
+    scanf("%lf",&angulo);
+    printf("Qual o valor da gravidade\n");
+    scanf("%lf",&gravidade);
+    angulo = formula_cos(angulo);
+    velocidade_horizontal = formula_velocidade_horizontal(velocidade_inical,angulo);
+    velocidade_vertical = formula_velocidade_vertical(velocidade_inical,angulo);
+    tempo = formula_tempo_subida(velocidade_vertical,gravidade);
+    tempo = formula_delta_tempo(tempo);
+    distancia = formula_distancia(velocidade_horizontal,tempo,angulo);
+    printf("Resultado é: %.2lf\n",distancia);
+    break;
+    
     }
     return 0;
-}*/
-float main(){
-    float velocidade_inicial, velocidade_inicial_y, velocidade_inicial_x, distancia_total_x;
-    float distancia_total_y, gravidade, angulo_x, angulo_y, resultado;
-    int angulo, tempo, escolha;
-        
-        printf("Qual será o angulo do lançamento? \n");
-            scanf("%d", &angulo);
-                switch (angulo)
-                {
-                    case 30:
-                    printf("O que vc quer descobrir, caso seja o tempo de subida digite 1, altura maxima digite 2 e distancia 3:");
-                        scanf("%d", &escolha);
-                    switch (escolha)
-                    {
-                        case 1:
-                            printf("Qual o valor da velocidade inicial?\n");
-                                scanf("%f",&velocidade_inicial);
-                            printf("Qual o valor da gravidade?\n");
-                                scanf("%f",&gravidade);
-                                    velocidade_inicial_y = velocidade_inicial*0.5;
-                                        tempo = velocidade_inicial_y/gravidade;
-                                            printf("O valor valor do tempo é %d m/s",tempo);
-                        break;
-                        
-                        case 2:
-                            printf("Qual o valor da velocidade inicial?\n");
-                                scanf("%f",&velocidade_inicial);
-                            printf("Qual o valor da gravidade?\n");
-                                scanf("%f",&gravidade);
-                                    velocidade_inicial_y = velocidade_inicial*0.5;
-                                        distancia_total_y = (velocidade_inicial_y*velocidade_inicial_y)/(2*gravidade);
-                                            printf("O valor da distancia total y é %f m/s",distancia_total_y);
-                        break;
-                        
-                        case 3:
-                            printf("Qual o valor da velocidade inicial?\n");
-                                scanf("%f",&velocidade_inicial);
-                            printf("Qual o valor da gravidade?\n");
-                                scanf("%f",&gravidade);
-                                    velocidade_inicial_y = velocidade_inicial*0.5;
-                                        tempo = velocidade_inicial_y/gravidade;
-                                            velocidade_inicial_x = velocidade_inicial*0.87;
-                                                distancia_total_x = velocidade_inicial_x*(tempo*2);
-                                                    printf("O valor do alcanse é %f m/s",distancia_total_x);
-                    }
-                    
-                }
-                
-            }
+    }
+
 
 
